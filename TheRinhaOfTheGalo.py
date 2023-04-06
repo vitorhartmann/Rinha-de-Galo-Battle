658778c49aaf9baca041567bb988d21804dc61fc
Vitor Hartmann <32484028+vitorhartmann@users.noreply.github.com>
2023-04-05 23:07:18 -0300

    Stable

--- TheRinhaOfTheGalo.py
+++ TheRinhaOfTheGalo.py
@@ -12,9 +12,27 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Rinha de Galo")
 background = pygame.image.load(f"Imagens/PlanoDeFundoALT.png")
 
+global terrenos
+global fundo
+
+terrenos = {
+    "Favela": {"Arma": 1.1, "Calca": 0.9, "Tenis": 0.9},
+    "Loja de Roupas": {"Arma": 0.9, "Calca": 1.1, "Tenis": 0.9},
+    "Quadra de Esportes": {"Arma": 0.9, "Calca": 0.9, "Tenis": 1.1}
+}
+
+fundos = [
+    pygame.image.load(f"Imagens/Favela.png"),
+    pygame.image.load(f"Imagens/Loja.png"),
+    pygame.image.load(f"Imagens/Quadra.png")
+]
+
+terreno_escolhido = random.choice(list(terrenos.keys()))
+fundo = fundos[list(terrenos.keys()).index(terreno_escolhido)]
+
 
 class Galo:
-    #Definindo o que representa um galo e seus atributos.
+    # Definindo o que representa um galo e seus atributos.
     def __init__(self, name, type, hp, attack):
         self.name = name
         self.type = type
@@ -28,32 +46,36 @@ class Galo:
 
         if name == "Galo de Arma":
             self.attacks = [
-                {"name": "Bicada - 10", "power": 10},
-                {"name": "Tiro - 15", "power": 15},
+                {"name": "Bicada", "power": 10},
+                {"name": "Tiro", "power": 15},
                 {"name": "Ataque Especial", "power": 15},
                 {"name": "Aumentar ataque", "power": 0}
             ]
         elif name == "Galo de Calca":
             self.attacks = [
-                {"name": "Bicada - 10", "power": 10},
-                {"name": "Calçada - 15", "power": 15},
+                {"name": "Bicada", "power": 10},
+                {"name": "Calçada", "power": 15},
                 {"name": "Ataque Especial", "power": 15},
                 {"name": "Aumentar ataque", "power": 0}
             ]
         elif name == "Galo de Tenis":
             self.attacks = [
-                {"name": "Bicada - 10", "power": 10},
-                {"name": "Chute - 15", "power": 15},
+                {"name": "Bicada", "power": 10},
+                {"name": "Chute", "power": 15},
                 {"name": "Ataque Especial", "power": 15},
                 {"name": "Aumentar ataque", "power": 0}
             ]
 
-#Tela de inicio, definição padrão básica
+# Tela de inicio, definição padrão básica
+
+
 def draw_text(screen, text, x, y):
     surface = font.render(text, True, (255, 255, 255))
     screen.blit(surface, (x, y))
 
-#Função de desenhar os galos na tela, tamanho, e de onde pegar a imagem.
+# Função de desenhar os galos na tela, tamanho, e de onde pegar a imagem.
+
+
 def draw_galo(screen, galo, x, y, size):
     image = pygame.image.load(f"Imagens/{galo.name}.png")
     image = pygame.transform.scale(image, (size, size))
@@ -67,7 +89,9 @@ def draw_galooponnent(screen, galo, x, y, size):
     image = pygame.transform.scale(image, (size, size))
     screen.blit(image, (x, y))
 
-#Função da barra de vida de forma visual
+# Função da barra de vida de forma visual
+
+
 def draw_health_bars(screen, player_galo, opponent_galo):
     # Barra de vida do jogador
     draw_text(screen, str(player_galo.hp), 100, 590)
@@ -83,22 +107,25 @@ def draw_health_bars(screen, player_galo, opponent_galo):
     pygame.draw.rect(screen, (0, 255, 0), (490, 220,
                      int(200 * health_percentage), 20))
 
-#Função de atualizar as barras de vida
+# Função de atualizar as barras de vida
+
+
 def update_health_bars(player_galo, opponent_galo):
     player_galo_hp = player_galo.hp
     opponent_galo_hp = opponent_galo.hp
     return player_galo_hp, opponent_galo_hp
 
-#Função para escrever no centro da tela uma mensagem
+# Função para escrever no centro da tela uma mensagem
+
+
 def draw_text_centered(screen, text):
-            surface = font.render(text, True, (255, 255, 255))
-            x = (screen.get_width() - surface.get_width()) // 2
-            y = (screen.get_height() - surface.get_height()) // 2
-            screen.blit(surface, (x, y))
+    surface = font.render(text, True, (255, 255, 255))
+    x = (screen.get_width() - surface.get_width()) // 2
+    y = (screen.get_height() - surface.get_height()) // 2
+    screen.blit(surface, (x, y))
 
 
-
-#Inicio da classe jogador
+# Inicio da classe jogador
 class Player:
     def __init__(self, galo):
         self.galo = galo
@@ -106,19 +133,15 @@ class Player:
 
 class Game:
 
-    #Modificadores de dano dos galos - Valores teste, são utilizados para multiplicar o dano referente à vantagem de tipo
+    # Modificadores de dano dos galos - Valores teste, são utilizados para multiplicar o dano referente à vantagem de tipo
     type_chart = {
         "Arma": {"Arma": 1, "Calca": 0.7, "Tenis": 1.3},
-        "Calca": {"Arma": 1.3, "Calca": 1, "Tenis": 0.7 },
+        "Calca": {"Arma": 1.3, "Calca": 1, "Tenis": 0.7},
         "Tenis": {"Arma": 0.7, "Calca": 1.3, "Tenis": 1}
     }
 
+    # Iniciando o jogador, e seus atributos
 
-
-    
-
-
-    #Iniciando o jogador, e seus atributos
     def __init__(self):
         self.player = None
         self.opponent = None
@@ -126,7 +149,7 @@ class Game:
         self.attack_selected = False
         self.selected_attack = None
 
-    #Definindo o galo do jogador (tela de seleção)
+    # Definindo o galo do jogador (tela de seleção)
     def select_galo(self):
         selected_galo = None
         while selected_galo is None:
@@ -170,19 +193,22 @@ class Game:
 
         # "Inteligência artificial" escolhendo o galo
             pygame.display.update()
+        screen.blit(background, (0, 0))
         draw_text(screen, "Oponente Escolhendo.", 200, 400)
         pygame.display.update()
         pygame.time.delay(1000)
+        screen.blit(background, (0, 0))
         draw_text(screen, "Oponente Escolhendo..", 200, 400)
         pygame.display.update()
         pygame.time.delay(1000)
+        screen.blit(background, (0, 0))
         draw_text(screen, "Oponente Escolhendo...", 200, 400)
         pygame.display.update()
         pygame.time.delay(2000)
-        
+
         return Player(selected_galo)
 
-    #Função de escolher o galo do oponente
+    # Função de escolher o galo do oponente
     def select_opponent(self):
         opponents = [
             Galo("Galo de Arma", "Arma", 100, 20),
@@ -193,7 +219,8 @@ class Game:
         # Cria uma lista ordenada de tipos de galos, do mais forte ao mais fraco
         types_ordered = ["Arma", "Calca", "Tenis"]
         player_type = self.player.galo.type
-        types_ordered.remove(player_type) # remove o tipo de galo do jogador da lista
+        # remove o tipo de galo do jogador da lista
+        types_ordered.remove(player_type)
 
         # Seleciona o tipo de galo mais forte para combater o tipo do jogador
         opponent_type = None
@@ -212,16 +239,14 @@ class Game:
 
         return Player(opponent_galo)
 
-    
-
     def select_attack(self):
         selected_attack = None
         while selected_attack is None:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                
-            screen.blit(background, (0, 0))
+
+            screen.blit(fundo, (0, 0))
             draw_text(screen, f"{self.player.galo.name} ataca:", 50, 50)
             draw_galo(screen, self.player.galo, 50, 320, 200)
             draw_galooponnent(screen, self.opponent.galo, 500, 20, 200)
@@ -245,22 +270,12 @@ class Game:
     def fight(self):
         clock = pygame.time.Clock()
 
-
-            #Modificadores de terrenos - São utilizados para multiplicador o dano referente ao terreno atual
-        terrenos = {
-        "Favela": {"Arma": 1.1, "Calca": 0.9, "Tenis": 0.9},
-        "Loja de Roupas": {"Arma": 0.9, "Calca": 1.1, "Tenis": 0.9 },
-        "Quadra de Esportes": {"Arma": 0.9, "Calca": 0.9, "Tenis": 1.1}
-        }
-
-        terreno_escolhido = random.choice(list(terrenos.keys()))
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
 
-            screen.blit(background, (0, 0))
+            screen.blit(fundo, (0, 0))
 
             if self.player_turn:
                 draw_text(screen, "Sua vez de atacar", 50, 50)
@@ -269,37 +284,43 @@ class Game:
                 draw_health_bars(screen, self.player.galo, self.opponent.galo)
 
                 # exibe os ataques disponíveis
-                for i, attack in enumerate(self.player.galo.attacks):
-                    text = f"{i + 1}. {attack['name']}"
-                    draw_text(screen, text, 250, 200 + i * 50)
 
                 if not self.attack_selected:
                     self.select_attack()  # seleciona o ataque apenas uma vez
                 else:
                     draw_text(
-                        screen, f"Você usou {self.selected_attack['name']}", 50, 400)
+                        screen, f"Você usou {self.selected_attack['name']}", 50, 100)
                     if self.selected_attack['name'] == "Ataque Especial":
                         attack_modifier = Game.type_chart[self.player.galo.type][self.opponent.galo.type]
-                    
-                    else:
-                       attack_modifier = 1 
 
-                    
+                    else:
+                        attack_modifier = 1
+
                     terreno_modifier = terrenos[terreno_escolhido][self.player.galo.type]
 
-                    self.opponent.galo.hp -= int(self.selected_attack['power'] * attack_modifier * terreno_modifier)
+                    draw_text(screen, f"Você causou " + str(int(
+                        self.selected_attack['power'] * attack_modifier * terreno_modifier)) + " de dano", 50, 150)
+                    pygame.display.update()
+                    pygame.time.delay(2000)
+
+                    self.opponent.galo.hp -= int(
+                        self.selected_attack['power'] * attack_modifier * terreno_modifier)
 
                     if self.opponent.galo.hp <= 0:
                         self.opponent.galo.hp = 0
 
                         screen.fill((0, 0, 0))
                         screen.blit(background, (0, 0))
+                        screen.blit(fundo, (0, 0))
                         draw_text(
-                        screen, f"{self.opponent.galo.name} desmaiou e saiu de combate", 250, 50)
+                            screen, f"{self.opponent.galo.name} desmaiou e saiu de combate", 250, 50)
                         draw_galo(screen, self.player.galo, 50, 320, 200)
-                        draw_galooponnent(screen, self.opponent.galo, 500, 20, 200)
-                        draw_health_bars(screen, self.player.galo, self.opponent.galo)
-                        draw_health_bars(screen, self.player.galo, self.opponent.galo)
+                        draw_galooponnent(
+                            screen, self.opponent.galo, 500, 20, 200)
+                        draw_health_bars(
+                            screen, self.player.galo, self.opponent.galo)
+                        draw_health_bars(
+                            screen, self.player.galo, self.opponent.galo)
                         pygame.display.update()
                         pygame.time.delay(4000)
                         screen.fill((0, 0, 0))
@@ -310,7 +331,6 @@ class Game:
                         return
                     self.player_turn = False
                     self.attack_selected = False
-
 
                     pygame.time.delay(500)  # adiciona um delay de 500 ms
                     pygame.display.update()
@@ -326,7 +346,7 @@ class Game:
                 opponent_attack = random.choice(self.opponent.galo.attacks)
                 draw_text(
                     screen, f"O oponente usou {opponent_attack['name']}", 50, 100)
-                
+
                 if opponent_attack['name'] == "Ataque Especial":
                     attack_modifier = Game.type_chart[self.opponent.galo.type][self.player.galo.type]
                 else:
@@ -334,31 +354,33 @@ class Game:
 
                 terreno_modifier = terrenos[terreno_escolhido][self.opponent.galo.type]
 
-                self.player.galo.hp -= int(opponent_attack['power'] * attack_modifier * terreno_modifier)
-
+                draw_text(screen, f"Oponente causou " + str(int(
+                    opponent_attack['power'] * attack_modifier * terreno_modifier)) + " de dano", 50, 150)
+                pygame.display.update()
+                self.player.galo.hp -= int(
+                    opponent_attack['power'] * attack_modifier * terreno_modifier)
 
                 if self.player.galo.hp <= 0:
-                        self.player.galo.hp = 0
+                    self.player.galo.hp = 0
 
-                        screen.fill((0, 0, 0))
-                        screen.blit(background, (0, 0))
-                        draw_text(
+                    screen.fill((0, 0, 0))
+                    screen.blit(background, (0, 0))
+                    screen.blit(fundo, (0, 0))
+                    draw_text(
                         screen, f"{self.player.galo.name} desmaiou e saiu de combate", 50, 200)
-                        draw_galo(screen, self.player.galo, 50, 320, 200)
-                        draw_galooponnent(screen, self.opponent.galo, 500, 20, 200)
-                        draw_health_bars(screen, self.player.galo, self.opponent.galo)
-                        pygame.display.update()
-                        pygame.time.delay(4000)
-                        screen.fill((0, 0, 0))
-                        pygame.display.update()
-                        draw_text_centered(screen, "Você Perdeu!")
-                        pygame.display.update()
-                        pygame.time.wait(2000)
-                        return
-                
-                draw_galo(screen, self.player.galo, 50, 320, 200)
-                draw_galooponnent(screen, self.opponent.galo, 500, 20, 200)
-                draw_health_bars(screen, self.player.galo, self.opponent.galo)
+                    draw_galo(screen, self.player.galo, 50, 320, 200)
+                    draw_galooponnent(screen, self.opponent.galo, 500, 20, 200)
+                    draw_health_bars(screen, self.player.galo,
+                                     self.opponent.galo)
+                    pygame.display.update()
+                    pygame.time.delay(4000)
+                    screen.fill((0, 0, 0))
+                    pygame.display.update()
+                    draw_text_centered(screen, "Você Perdeu!")
+                    pygame.display.update()
+                    pygame.time.wait(2000)
+                    return
+
                 pygame.display.update()
 
                 pygame.time.delay(1500)  # adiciona um delay de 500 ms
@@ -370,7 +392,6 @@ class Game:
             self.player.galo.hp = player_galo_hp
             self.opponent.galo.hp = opponent_galo_hp
             pygame.display.update()
-            
 
             # verifica se o jogo acabou
             if self.player.galo.hp <= 0:
@@ -380,10 +401,8 @@ class Game:
                 pygame.display.update()
                 return
 
-
             pygame.display.update()
             clock.tick(30)
-
 
 
 def main():
@@ -414,7 +433,6 @@ def main():
         game.fight()
 
     pygame.quit()
-
 
 
 if __name__ == "__main__":
